970ac8c42c4ea915ddd869dd9fd2eacdccf25065
dramforever <dramforever@live.com>
2023-11-16 23:24:03 +0800

    exercise 1

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -5,18 +5,14 @@
 use axstd::println;
 
 const PLASH_START: usize = 0x22000000;
+const PLASH_SIZE: usize = 32 << 20; // 32 MiB
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    let apps_start = PLASH_START as *const u8;
-    let apps_size = 32; // Dangerous!!! We need to get accurate size of apps.
-    println!("Load payload ...");
-    let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
-    println!("content: {:#x}", bytes_to_usize(&code[..8]));
-    println!("Load payload ok!");
-}
+    let pflash = unsafe { core::slice::from_raw_parts(PLASH_START as *const u8, PLASH_SIZE) };
 
-#[inline]
-fn bytes_to_usize(bytes: &[u8]) -> usize {
-    usize::from_be_bytes(bytes.try_into().unwrap())
+    let entry = cpio_reader::iter_files(pflash).next().unwrap();
+    assert!(entry.mode().contains(cpio_reader::Mode::REGULAR_FILE));
+    let code = entry.file();
+    println!("Payload {} is {:02x?}", entry.name(), code);
 }
