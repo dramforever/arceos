e53100dc115d8ab5c3390c45ccb486af317582b9
dramforever <dramforever@live.com>
2023-11-16 23:24:32 +0800

    exercise 2

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -11,8 +11,9 @@ const PLASH_SIZE: usize = 32 << 20; // 32 MiB
 fn main() {
     let pflash = unsafe { core::slice::from_raw_parts(PLASH_START as *const u8, PLASH_SIZE) };
 
-    let entry = cpio_reader::iter_files(pflash).next().unwrap();
-    assert!(entry.mode().contains(cpio_reader::Mode::REGULAR_FILE));
-    let code = entry.file();
-    println!("Payload {} is {:02x?}", entry.name(), code);
+    for entry in cpio_reader::iter_files(pflash) {
+        assert!(entry.mode().contains(cpio_reader::Mode::REGULAR_FILE));
+        let code = entry.file();
+        println!("Payload {} is {:02x?}", entry.name(), code);
+    }
 }
