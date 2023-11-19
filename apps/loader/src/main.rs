71e075b0e4538f5318957abc8743c34eafbffc22
dramforever <dramforever@live.com>
2023-11-19 15:51:10 +0800

    step 5

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -5,10 +5,36 @@
 #[cfg(feature = "axstd")]
 use axstd::println;
 
+//
+// App aspace
+//
+#[link_section = ".data.app_page_table"]
+static mut APP_PT_SV39: [u64; 512] = [0; 512];
+
+unsafe fn init_app_page_table() {
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+    // 0x0000_0000..0x4000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0] = (0x00000 << 10) | 0xef;
+    // For App aspace!
+    // 0x4000_0000..0x8000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[1] = (0x80000 << 10) | 0xef;
+}
+
+unsafe fn switch_app_aspace() {
+    use riscv::register::satp;
+    let page_table_root = APP_PT_SV39.as_ptr() as usize - axconfig::PHYS_VIRT_OFFSET;
+    satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+    riscv::asm::sfence_vma_all();
+}
+
 const PLASH_START: usize = 0x22000000;
 
 const SYS_HELLO: usize = 1;
 const SYS_PUTCHAR: usize = 2;
+
 static mut ABI_TABLE: [usize; 16] = [0; 16];
 
 fn register_abi(num: usize, handle: usize) {
@@ -43,10 +69,12 @@ fn main() {
         load_code.as_ptr()
     );
 
-    // app running aspace
-    // SBI(0x80000000) -> App <- Kernel(0x80200000)
-    // 0xffff_ffc0_0000_0000
-    const RUN_START: usize = 0xffff_ffc0_8010_0000;
+    unsafe {
+        init_app_page_table();
+        switch_app_aspace();
+    }
+
+    const RUN_START: usize = 0x40100000;
     let run_code = unsafe { core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size) };
 
     run_code.copy_from_slice(load_code);
@@ -66,5 +94,4 @@ fn main() {
         abi_table = sym ABI_TABLE,
         )
     }
-
 }
